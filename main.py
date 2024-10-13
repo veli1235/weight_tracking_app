914eafa215db0ebcebdfb15deb647e4e6132f9cc
veli1235 <gnlveliyeva1128@gmail.com>
2024-10-13 13:37:02 +0400

    end

--- main.py
+++ main.py
@@ -20,3 +20,18 @@ def get_user(username: str, db: Session = Depends(get_db)):
 def create_user(item : USerCreateSchema, db:Session = Depends(get_db)):
     message = create_user_in_db(data=item,db=db)
     return message
+
+@app.post("/weight")
+def create_weight(username: str,item : UserCreateWeight, db : Session = Depends(get_db)):
+    message = create_weight_in_db(username = username,data = item, db = db)
+    return message
+
+@app.get("/weight_diff")
+def change_weight(username: str, db: Session = Depends(get_db)):
+    message = get_weight_change_from_db(username = username, db = db)
+    return message
+
+@app.get("/weight")
+def calculate_bmi(username: str, db: Session = Depends(get_db)):
+    message = calculate_bmi_for_last_weight(username = username, db = db)
+    return message
